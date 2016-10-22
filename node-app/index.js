06a67e91ab4daca8f1d0bcbd4b9dfd8d8feeb9f4
walid <eng_walid_ashraf@hotmail.com>
2016-10-22 03:55:14 -0700

    simple node app

---
+++ node-app/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+// Constants
+var PORT = 8080;
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello Docker Cairo Geeks\n');
+});
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
